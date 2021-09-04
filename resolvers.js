3b7f58509ef4008453b427897c294cf021f00e31
Titanio Yudista <titanioyudista98@gmail.com>
2021-09-04 09:35:34 +0700

    mutation delete, update and create graphql

--- resolvers.js
+++ resolvers.js
@@ -5,7 +5,21 @@ module.exports = {
         getAllBooks: async () => await BookModel.find({})
     },
 
-    // Mutation: {
+    Mutation: {
+        createBook: async (_, args) => {
+            const book = new BookModel(args);
+            await book.save()
+            return book
+        },
 
-    // }
+        updateBook: async (_, args) => {
+            const book = await BookModel.findByIdAndUpdate(args._id, args, { new: true })
+            return book
+        },
+
+        deleteBook: async (_, args) => {
+            const book = await BookModel.findByIdAndRemove(args._id)
+            return book ? true : false
+        }
+    }
 }
--- schema.js
+++ schema.js
@@ -14,6 +14,28 @@ const typeDefs = gql`
         getAllBooks: [Book]!
     }
 
+    type Mutation {
+        createBook(
+            title: String!,
+            author: String!,
+            description: String,
+            release_year: Int!,
+            genre: String!
+        ): Book!
+
+        updateBook(
+            _id: ID!,
+            title: String,
+            author: String,
+            description: String,
+            release_year: Int,
+            genre: String
+        ): Book!
+
+        deleteBook(
+            _id: ID!
+        ): Boolean
+    }
 `;
 
 module.exports = typeDefs
